6c7cc0ccd87554fe3c7eecb996a3e2e08094196b
Иван <eretikasgaarda_zamakiller@bk.ru>
2026-07-17 19:42:19 +0700

    итог2

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,6 @@ public class Main {
         printLeapYearStatus(year);
     }
 
-
     private static void printLeapYearStatus(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
             System.out.println(year + " год — високосный год");
